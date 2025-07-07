d9b8d404e6d9df1b3f2254ea00719b17e4569de3
RanelShine <ranelleshine076@gmail.com>
2025-07-07 09:07:38 +0100

    okay

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-d5m_z^)cm0!amfbutoebq88vi6p3#t*%@d5#d7sy5*ngv2pj^@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['nguetsa.pythonanywhere.com', '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,15 +104,26 @@ TIME_ZONE = 'Africa/Douala'
 #         'PORT': '5432',
 #     }
 # }
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
     'OPTIONS': {
             'timeout': 20,
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     },
+#     'OPTIONS': {
+#             'timeout': 20,
+#     }
+# }
 
 
 # Password validation
